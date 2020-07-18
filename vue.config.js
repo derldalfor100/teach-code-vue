79c0b1d35d813063fdea4b3b818c8f3fa5641f05
derldalfor100 <derldalfor100@gmail.com>
2020-07-18 16:41:26 +0300

    add public path config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 process.env.VUE_APP_VERSION = require('./package.json').version;
 
+const local = process.argv && process.argv.indexOf('--local') > -1;
+
 module.exports = {
   // config
+  publicPath:  !local && process.env.NODE_ENV === 'production' ? "https://derldalfor100.github.io/teach-code-vue" : ''
 }
