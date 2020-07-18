74dd32853e98c43e776c7e772ca6bdba27814001
derldalfor100 <derldalfor100@gmail.com>
2020-07-18 20:37:20 +0300

    fix public path

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ process.env.VUE_APP_VERSION = require('./package.json').version;
 const local = process.argv && process.argv.indexOf('--local') > -1;
 
 module.exports = {
-  publicPath: !local && process.env.NODE_ENV === 'production' ? "https://derldalfor100.github.io/teach-code-vue" : '',
+  publicPath: !local && process.env.NODE_ENV === 'production' ? "/teach-code-vue" : '',
   css: {
     loaderOptions: {
       sass: {
