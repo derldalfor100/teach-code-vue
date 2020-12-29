c04a3b917f08680b1119ca659d85164a4614c4e2
derldalfor100 <derldalfor100@gmail.com>
2020-12-29 21:10:14 +0200

    remove buttonTest

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,9 +3,6 @@
     <h1>This is an about page</h1>
     <img alt="View" src="../assets/images/view.jpg" />
     <Alert :msg="'Just a message'" />
-    <ProvideInject>
-      <ButtonTest />
-    </ProvideInject>
     <ProvideInjectTheme>
       <ButtonTheme />
     </ProvideInjectTheme>
@@ -21,8 +18,6 @@ h1 {
 <script lang="ts">
 import Vue from "vue";
 import Alert from "@/components/Alert.vue";
-import ProvideInject from "@/components/ProvideInject.vue";
-import ButtonTest from "@/components/ButtonTest.vue";
 import ProvideInjectTheme from '@/components/ProvideInjectTheme.vue';
 import ButtonTheme from '@/components/ButtonTheme.vue';
 
@@ -35,8 +30,6 @@ export default Vue.extend({
   },
   components: {
     Alert,
-    ProvideInject,
-    ButtonTest,
     ProvideInjectTheme,
     ButtonTheme
   },
